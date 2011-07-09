6a6aabd287403b88b5f961a6be40421bcf8a6b18
Andrew Heiss <andrewheiss@gmail.com>
2011-07-08 22:11:52 -0600

    Whoa. Massive lazy commit.

---
+++ layout.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<title>Template</title>
+
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+
+	<link rel="stylesheet" href="css/template.css" />
+	<link rel="stylesheet" href="css/formalize.css" />
+
+	<!--[if lt IE 9]>
+	<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head>
+
+<body id="home">
+	<div id="wrapper">
+		<section id="top">
+			<header class="column grid_9">
+				<h1>
+					<a href="#"><img src="images/MPAA-logo.png" alt="MPAA Logo" /></a>
+					<span>BYU MPA Association</span>
+				</h1>
+			</header>
+		</section>
+			
+		<section id="nav-wrapper">
+			<nav class="column grid_8">
+				<ul>
+					<li><a href="#"><img src="images/home-button.png" width="17" height="13" alt="Home Button" /></a></li>
+					<li><a href="#">News</a></li>
+					<li><a href="#">Contact</a></li>
+					<li><a href="#">Alumni</a></li>
+					<li><a href="#">Blog</a></li>
+					<li><a href="#">The Caucus</a></li>
+				</ul>
+			</nav>
+			<div id="search-box" class="column grid_4">
+				<input type="search" name="search" value="" placeholder="Search&hellip;" />
+				<ul class="social-icons">
+					<li><a href="#"><img src="images/social/16/facebook.png" alt="Facebook" /></a></li>
+					<li><a href="#"><img src="images/social/16/twitter.png" alt="Twitter" /></a></li>
+					<li><a href="#"><img src="images/social/16/linkedin.png" alt="LinkedIn" /></a></li>
+					<li><a href="#"><img src="images/social/16/youtube.png" alt="YouTube" /></a></li>
+					<li><a href="#"><img src="images/social/16/flickr.png" alt="Flickr" /></a></li>	
+				</ul>
+			</div>
+		</section>
+		
+		<section id="highlight">
+			<article class="column grid_4">
+				<div class="inset">
+					<h2>This is the best club website in the history of club websites</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+				</div>
+			</article>
+			<figure class="column grid_8">
+				<img src="images/ymountain.jpg" width="690" height="250" alt="Ymountain" />
+			</figure>
+		</section>
+		
+		<section id="content">
+			<div class="column grid_6">
+				<h2>Here's a column</h2>
+			</div>
+			<div class="column grid_6">
+				<h2>And here's another column</h2>
+			</div>
+		</section>
+		
+		<section id="bottom">
+			<footer>
+				<div class="column grid_4">
+					<h3>Big footer thing</h3>
+				</div>
+				<div class="column grid_4">
+					<h3>Big footer thing</h3>
+				</div>
+				<div class="column grid_4">
+					<h3>Big footer thing</h3>
+				</div>
+			</footer>
+		</section>
+	</div> <!-- End of #wrapper -->
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+	<script src="js/jquery.formalize.min.js"></script>
+</body>
+</html>
