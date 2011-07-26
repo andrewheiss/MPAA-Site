bea281b82470414d207cd0f425c5a574cd71834e
Andrew Heiss <andrewheiss@gmail.com>
2011-07-25 23:18:01 -0600

    Forgot to include this in the previous commit… :)

---
+++ user-login.tpl.php
@@ -0,0 +1,8 @@
+<p>If you don't have an account at one of the above sites, you can create a new <?php print variable_get('site_name', ''); ?> account and use it to log in below.<sup>*</sup>
+<br />
+<small><em>*It will be far more convenient to create an account using one of the sites above, though. You'll have one fewer username and password to remember.</em></small></p>
+
+<fieldset class="collapsible collapsed">
+	<legend><a href="#">Login with an MPAA site account</a></legend>
+	<?php print $rendered; ?>
+</fieldset>
